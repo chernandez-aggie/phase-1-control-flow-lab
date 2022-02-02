7c1578d5bb841ecee5d25a62d8ceb354f42c7805
candacehernandez <chernandez.aggie@gmail.com>
2022-02-01 22:06:54 -0600

    commit tree 02074ce41206861bb2496a847bad85544027636e

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet < 400){
+    return 'This one is on me!';
+
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+
+  } else if (distanceInFeet > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good.": "No go."
 }
+//const tip;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
